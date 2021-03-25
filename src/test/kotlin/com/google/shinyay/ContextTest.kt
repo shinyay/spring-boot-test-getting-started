441f0c764f91916dac0f6b5fed3b4e5f7d1a79d4
shinyay <shinya.com@gmail.com>
2021-03-25 11:38:46 +0900

    Add Health check for Hello function

--- src/test/kotlin/com/google/shinyay/ContextTest.kt
+++ src/test/kotlin/com/google/shinyay/ContextTest.kt
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ContextTests(@Autowired val helloController: HelloController) {
+class ContextTest(@Autowired val helloController: HelloController) {
 
 	@Test
 	fun contextLoads() {
