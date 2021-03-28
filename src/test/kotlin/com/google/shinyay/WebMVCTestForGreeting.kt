5ab426cf6c90b8889c1a2402409aaa8d662967a6
shinyay <shinya.com@gmail.com>
2021-03-28 22:04:46 +0900

    Modify import to optimize

--- src/test/kotlin/com/google/shinyay/WebMVCTestForGreeting.kt
+++ src/test/kotlin/com/google/shinyay/WebMVCTestForGreeting.kt
@@ -2,19 +2,29 @@ package com.google.shinyay
 
 import com.google.shinyay.controller.GreetingController
 import com.google.shinyay.service.GreetingService
+import org.hamcrest.Matchers.containsString
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(GreetingController::class)
 class WebMVCTestForGreeting(
-    val mockMVC: MockMvc,
+    private val mockMvc: MockMvc,
     @MockBean val service: GreetingService
 ) {
     @Test
     fun greetingShouldReturnMessageFromService() {
-        `when`(service.hello()).thenReturn("Hello")
+        `when`(service.hello()).thenReturn("Hell")
+
+        mockMvc.perform(get("/greeting"))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(content().string(containsString("Hello")))
     }
 }
