caaed3f1c796ea383ca17175d28c8d5deba28364
shinyay <shinya.com@gmail.com>
2021-03-28 21:45:36 +0900

    Add Greeting Service as MockBean

--- src/test/kotlin/com/google/shinyay/WebMVCTestForGreeting.kt
+++ src/test/kotlin/com/google/shinyay/WebMVCTestForGreeting.kt
@@ -1,9 +1,15 @@
 package com.google.shinyay
 
 import com.google.shinyay.controller.GreetingController
+import com.google.shinyay.service.GreetingService
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 
 @WebMvcTest(GreetingController::class)
-class WebMVCTestForGreeting(val mockMVC: MockMvc) {
+class WebMVCTestForGreeting(
+    val mockMVC: MockMvc,
+    @MockBean val service: GreetingService
+) {
+    
 }
