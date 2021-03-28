c855db328eda9feeb7de2ff96a81204d0a9e85c6
shinyay <shinya.com@gmail.com>
2021-03-28 21:42:26 +0900

    Add MockMVC by Constructor Injection

--- src/test/kotlin/com/google/shinyay/WebMVCTestForGreeting.kt
+++ src/test/kotlin/com/google/shinyay/WebMVCTestForGreeting.kt
@@ -2,7 +2,8 @@ package com.google.shinyay
 
 import com.google.shinyay.controller.GreetingController
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
 
 @WebMvcTest(GreetingController::class)
-class WebMVCTestForGreeting {
+class WebMVCTestForGreeting(val mockMVC: MockMvc) {
 }
