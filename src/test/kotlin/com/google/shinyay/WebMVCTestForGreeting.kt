beeff32295ee9e5aa7ca92d129da98ded587fd24
shinyay <shinya.com@gmail.com>
2021-03-28 21:53:13 +0900

    Add stubbing with Mockito

--- src/test/kotlin/com/google/shinyay/WebMVCTestForGreeting.kt
+++ src/test/kotlin/com/google/shinyay/WebMVCTestForGreeting.kt
@@ -3,6 +3,7 @@ package com.google.shinyay
 import com.google.shinyay.controller.GreetingController
 import com.google.shinyay.service.GreetingService
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
@@ -14,6 +15,6 @@ class WebMVCTestForGreeting(
 ) {
     @Test
     fun greetingShouldReturnMessageFromService() {
-        
+        `when`(service.hello()).thenReturn("Hello")
     }
 }
