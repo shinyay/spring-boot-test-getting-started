1b6713e90087b6471671958007625c64c2a8be87
shinyay <shinya.com@gmail.com>
2021-03-28 21:46:57 +0900

    Add test snipet

--- src/test/kotlin/com/google/shinyay/WebMVCTestForGreeting.kt
+++ src/test/kotlin/com/google/shinyay/WebMVCTestForGreeting.kt
@@ -2,6 +2,7 @@ package com.google.shinyay
 
 import com.google.shinyay.controller.GreetingController
 import com.google.shinyay.service.GreetingService
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
@@ -11,5 +12,8 @@ class WebMVCTestForGreeting(
     val mockMVC: MockMvc,
     @MockBean val service: GreetingService
 ) {
-    
+    @Test
+    fun greetingShouldReturnMessageFromService() {
+        
+    }
 }
