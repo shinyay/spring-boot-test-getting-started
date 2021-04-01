386327084180136947515b0eb974d8a71c2c97d8
shinyay <shinya.com@gmail.com>
2021-04-01 23:05:39 +0900

    add HTTP Status verification

--- src/test/kotlin/com/google/shinyay/ViewControllerTest.kt
+++ src/test/kotlin/com/google/shinyay/ViewControllerTest.kt
@@ -1,10 +1,20 @@
 package com.google.shinyay
 
 import com.google.shinyay.controller.ViewController
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(controllers = [ViewController::class])
 class ViewControllerTest(@Autowired val mockMvc: MockMvc) {
+
+    @Test
+    fun homePage() {
+        mockMvc.perform(
+            get("/home")
+        ).andExpect(status().is2xxSuccessful)
+    }
 }
