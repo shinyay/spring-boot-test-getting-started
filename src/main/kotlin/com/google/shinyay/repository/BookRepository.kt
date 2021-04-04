cba50b08e0eac38bd1de89a189b0eef86e865270
shinyay <shinya.com@gmail.com>
2021-04-04 10:13:17 +0900

    Add find function by author for order by price

--- src/main/kotlin/com/google/shinyay/repository/BookRepository.kt
+++ src/main/kotlin/com/google/shinyay/repository/BookRepository.kt
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository
 interface BookRepository: JpaRepository<Book, Long> {
 
     fun findAllOrderByPrice(): List<Book>
+
+    fun findAllByAuthorOrderByPrice(author: String): List<Book>
 }
