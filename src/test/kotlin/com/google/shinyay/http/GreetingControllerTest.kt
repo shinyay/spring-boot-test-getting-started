c1911682732fbc8e67dfc74b38c92ebcce9b01d0
shinyay <shinya.com@gmail.com>
2021-04-07 11:17:20 +0900

    Add RestClientTest class

--- src/test/kotlin/com/google/shinyay/http/GreetingControllerTest.kt
+++ src/test/kotlin/com/google/shinyay/http/GreetingControllerTest.kt
@@ -1,4 +1,13 @@
 package com.google.shinyay.http
 
-class GreetingControllerTest {
+import com.google.shinyay.controller.GreetingController
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest
+import org.springframework.test.web.client.MockRestServiceServer
+
+@RestClientTest(GreetingController::class)
+class GreetingControllerTest(
+    @Autowired val mockServer: MockRestServiceServer,
+    val greetingController: GreetingController
+) {
 }
