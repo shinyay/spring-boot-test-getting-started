66c4068469d3d13413c31e14aa9a83fee3b4783e
shinyay <shinya.com@gmail.com>
2021-04-09 11:22:46 +0900

    Add Member entity

---
+++ src/main/kotlin/com/google/shinyay/entity/Member.kt
@@ -0,0 +1,3 @@
+package com.google.shinyay.entity
+
+data class Member()
--- src/main/kotlin/com/google/shinyay/repository/MemberJdbcRepository.kt
+++ src/main/kotlin/com/google/shinyay/repository/MemberJdbcRepository.kt
@@ -1,8 +1,13 @@
 package com.google.shinyay.repository
 
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.queryForObject
 import org.springframework.stereotype.Repository
 
 @Repository
 class BookJdbcRepository(val jdbcTemplate: JdbcTemplate) {
+
+    fun getCountOfBooks() {
+        return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM book")
+    }
 }
