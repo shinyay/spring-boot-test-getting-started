b567ecd38ede4f4c55e8ece76d15a5cbc984ea93
shinyay <shinya.com@gmail.com>
2021-04-09 13:43:16 +0900

    Modify annotation using DataJdbcTest and Import

--- src/test/kotlin/com/google/shinyay/jdbc/MemberJdbcRepositoryTest.kt
+++ src/test/kotlin/com/google/shinyay/jdbc/MemberJdbcRepositoryTest.kt
@@ -4,15 +4,17 @@ import com.google.shinyay.repository.MemberJdbcRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.jdbc.Sql
 
-@JdbcTest
-@Sql(scripts = ["schema.sql", "data.sql"])
+@DataJdbcTest
+@Sql(scripts = ["classpath:schema.sql", "classpath:data.sql"])
+@Import(MemberJdbcRepository::class)
 class MemberJdbcRepositoryTest(@Autowired val memberJdbcRepository: MemberJdbcRepository) {
 
     @Test
     fun whenInjectInMemoryDataSource_thenReturnCorrectMemberCount() {
-        assertThat(memberJdbcRepository.getCountOfMembers()).isEqualTo(4)
+        assertThat(memberJdbcRepository.getCountOfMembers()).isEqualTo(3)
     }
 }
