3271b3f7fea466b003672d2647b7df15eff96583
shinyay <shinya.com@gmail.com>
2021-04-22 17:15:15 +0900

    Add function to display current time as Mono

--- src/main/kotlin/com/google/shinyay/service/GreetingService.kt
+++ src/main/kotlin/com/google/shinyay/service/GreetingService.kt
@@ -11,7 +11,7 @@ class GreetingService {
     fun hello() = "Hello"
 
     fun helloAtCurrentTime(): String {
-        val dateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:dd.ss.SSS")
+        val dateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
         val currentTime = ZonedDateTime.now(ZoneId.of("Japan")).format(dateTimeFormat)
         logger.info(currentTime)
         return "Hello at $currentTime"
