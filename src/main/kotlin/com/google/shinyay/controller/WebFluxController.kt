3f356943272013e12ce2d231329a95cc2bb548aa
shinyay <shinya.com@gmail.com>
2021-04-22 17:18:31 +0900

    Add function to display current time as Flux

--- src/main/kotlin/com/google/shinyay/controller/WebFluxController.kt
+++ src/main/kotlin/com/google/shinyay/controller/WebFluxController.kt
@@ -4,6 +4,7 @@ import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -16,5 +17,13 @@ class WebFluxController {
     @GetMapping("/monotime")
     fun currentTime():Mono<String> {
         return Mono.just(ZonedDateTime.now(ZoneId.of("Japan")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")))
+    }
+
+    @GetMapping("/fluxtime")
+    fun timeAndMessage(): Flux<String> {
+        return Flux.just("Current Time",
+            ZonedDateTime.now(ZoneId.of("Japan")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")),
+            "Now"
+        )
     }
 }
