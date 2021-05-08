cb1dc207ccac96de50a3c28eb6f05a06ce247562
shinyay <shinya.com@gmail.com>
2021-05-08 21:14:24 +0900

    Add same value test with Hamcrest

--- src/test/kotlin/com/google/shinyay/hamcrest/HamcrestAssertion.kt
+++ src/test/kotlin/com/google/shinyay/hamcrest/HamcrestAssertion.kt
@@ -1,4 +1,17 @@
 package com.google.shinyay.hamcrest
 
+import com.google.shinyay.service.HamcrestService
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
 class HamcrestAssertion {
+
+    private val hamcrestService = HamcrestService()
+
+    @Test
+    fun sameValue() {
+        assertThat(hamcrestService.returnStringValue(), `is`("foo"))
+    }
 }
